bd9b46ef5297ac5350a4b50f5d65db92d1c4df56
Ben <ben.buechler@stud.tu-darmstadt.de>
2026-07-26 20:47:23 +0200

    add Scaled suffix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.rodonisi.kover"
+    namespace = "com.rodonisi.kover.scaled"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.rodonisi.kover"
+        applicationId = "com.rodonisi.kover.scaled"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
